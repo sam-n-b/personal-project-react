60622367277cc8c07b72adb2c383cb7d5b1627f2
Sam-Bartos <sambartos@gmail.com>
2019-02-10 11:10:20 +1300

    added route dependencies

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {render} from 'react-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
 import Movie from './Movie'
+
 const App = () => {
   return (
     <div>
     <h1>React development has begun!</h1>
-    <Movie/>
+    <Movie propmovie = 'End of the tour'/>
+    
+    <Router>
+      <Route path='/movie' component={Movie}/>
+    </Router>
     </div>
   )
 }
--- client/components/Movie.jsx
+++ client/components/Movie.jsx
@@ -5,13 +5,16 @@ class Movie extends React.Component{
         super(props)
         this.state = {
             movie: 'Titanic',
-            year: 1997
+            year: 1997,
+            image: './images/titanic.jpg'
         }
     }
     render(){
         return(
             <div>
                 MOVIE PAGE!
+                <h1>{this.state.movie}</h1>
+                <h2>{this.props.propmovie}</h2>
             </div>
         )
     }
